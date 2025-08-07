08c5534491ea2167906c1407d6026c07225371a9
Yash Maheshwari <yashmaheshwari551@gmail.com>
2025-08-07 12:52:27 +0530

    Implemented: eslint rules for formatting and having consistency

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,27 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/no-deprecated-slot-attribute': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'vue/multi-word-component-names' : 'off'
+    'vue/multi-word-component-names' : 'off',
+    'array-bracket-spacing': ['error', 'never'],
+    'brace-style': ['error', '1tbs'],
+    'comma-spacing': ['error', { before: false, after: true }],
+    'eol-last': ['error', 'always'],
+    "indent": ['error', 2],
+    'key-spacing': ['error', { beforeColon: false, afterColon: true }],
+    'curly': 'all',
+    'no-multi-spaces': ['error', {
+      ignoreEOLComments: false,
+    }],
+    'computed-property-spacing': ['error', 'never'],
+    'func-call-spacing': ['error', 'never'],
+    'lines-around-comment': 'off',
+    'no-mixed-spaces-and-tabs': 'error',
+    'no-multiple-empty-lines': ['error', { max: 1 }],
+    'no-spaced-func': 'off',
+    'no-trailing-spaces': ['error'],
+    'object-curly-spacing': ['error', 'always'],
+    'quote-props': ['error', 'as-needed', { keywords: false, unnecessary: true, numbers: false }],
+    'quotes': ['error', 'double', { avoidEscape: true }],
   },
   overrides: [
     {
